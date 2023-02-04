7d0c8d464a3e0d3d7e6184d49e078deaefb274e0
gimminjae <min356812@naver.com>
2023-02-04 23:21:28 +0900

    글 삭제

--- frontend/src/pages/PostDetail.vue
+++ frontend/src/pages/PostDetail.vue
@@ -4,7 +4,7 @@
       <h1>{{ post.title }}</h1>
       <div class="d-flex gap-3">
         <router-link :to="{path: `/modify/${this.postId}`}" class="">수정</router-link>
-        <a class="">삭제</a>
+        <a class="" @click="remove_post" href="#">삭제</a>
       </div>
     </div>
     <div id="viewer" class="my-3"></div>
@@ -46,7 +46,25 @@ export default {
         el: document.querySelector('#viewer'),
         // 표시하고자 하는 내용은 여기에 들어간다.
       });
-    }
+    },
+    remove_post() {
+      if (!confirm('정말로 삭제하시겠습니까?')) {
+        return;
+      }
+      const jwt = VueCookies.get('access_token');
+
+      let config = {
+        headers: {
+          "Authorization": jwt
+        }
+      }
+      axios.delete(`/api/post/${this.postId}`, config)
+          .then(() => {
+            router.push({path: `/${VueCookies.get('nickname')}`});
+          }).catch(error => {
+        alert('글 삭제 실패 : ' + error.response.data);
+      })
+    },
   }
 }
 </script>
